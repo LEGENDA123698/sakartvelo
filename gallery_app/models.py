10ccc28efab98a3ccf2ac1bd25d0384b0de556e3
LEGENDA123698 <slavik.doronin@egmail.com>
2025-06-12 16:03:31 +0300

    i believe it's end_2(((

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='photos_for_gallery_media/', blank=True, null=True)
+    image = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='photos_for_gallery_media/', blank=True, null=True)
